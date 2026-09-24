68401a8bead83102284d187f41a10b28355a4261
Henrik Gregersen <henrik@bonest.dk>
2026-09-24 17:45:28 +0200

    winsuck 0.2.0

--- Formula/winsuck.rb
+++ Formula/winsuck.rb
@@ -1,16 +1,16 @@
 class Winsuck < Formula
   desc "Stream selected Windows NTFS files into WSL ext4"
   homepage "https://github.com/bonest/winsuck"
-  url "https://github.com/bonest/winsuck/releases/download/v0.1.0/winsuck_0.1.0_linux_amd64.tar.gz"
-  sha256 "395ab2bc339b9329a23b9826a3047be4f729e3b692324c8e08633015784788cc"
+  url "https://github.com/bonest/winsuck/releases/download/v0.2.0/winsuck_0.2.0_linux_amd64.tar.gz"
+  sha256 "2148d752030f17b6c3a5cbbb57447827f1d488c5d2852c489b2a47fd4b2dc507"
   license "MIT"
 
   skip_clean "bin/winsuck.exe"
 
   on_linux do
     resource "windows-sender" do
-      url "https://github.com/bonest/winsuck/releases/download/v0.1.0/winsuck_0.1.0_windows_amd64.zip"
-      sha256 "e4a94a6b37d64402134274b8f6548b930529955370f87dbe2a21422ddd5afb1d"
+      url "https://github.com/bonest/winsuck/releases/download/v0.2.0/winsuck_0.2.0_windows_amd64.zip"
+      sha256 "96b4044630ec97bb776b913ab48e35befc4e8f6a82feacff5b8ae22411d0f1e4"
     end
   end
 
