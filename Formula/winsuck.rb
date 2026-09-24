bf367f001b67eaaae474d9720c81bb2c0ed0e9c0
Henrik Gregersen <henrik@bonest.dk>
2026-09-24 16:04:39 +0200

    Add winsuck formula

---
+++ Formula/winsuck.rb
@@ -0,0 +1,27 @@
+class Winsuck < Formula
+  desc "Stream selected Windows NTFS files into WSL ext4"
+  homepage "https://github.com/bonest/winsuck"
+  url "https://github.com/bonest/winsuck/releases/download/v0.1.0/winsuck_0.1.0_linux_amd64.tar.gz"
+  sha256 "395ab2bc339b9329a23b9826a3047be4f729e3b692324c8e08633015784788cc"
+  license "MIT"
+
+  on_linux do
+    resource "windows-sender" do
+      url "https://github.com/bonest/winsuck/releases/download/v0.1.0/winsuck_0.1.0_windows_amd64.zip"
+      sha256 "e4a94a6b37d64402134274b8f6548b930529955370f87dbe2a21422ddd5afb1d"
+    end
+  end
+
+  def install
+    odie "winsuck supports WSL/Linux only" unless OS.linux?
+
+    bin.install "winsuck"
+    resource("windows-sender").stage do
+      bin.install "winsuck.exe"
+    end
+  end
+
+  test do
+    system "#{bin}/winsuck", "--version"
+  end
+end
